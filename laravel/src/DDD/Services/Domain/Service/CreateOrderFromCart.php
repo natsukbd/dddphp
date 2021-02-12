f094033fb8ad0077f3eab280892f54e1d13dc987
natsukbd <natsuki.pc@gmail.com>
2021-02-12 23:19:58 +0900

    供給されたカートから注文を作成するドメインサービス

---
+++ laravel/src/DDD/Services/Domain/Service/CreateOrderFromCart.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DDD\Services\Domain\Service;
+
+use DDD\Services\Domain\Model\Cart\Cart;
+
+final class CreateOrderFromCart
+{
+    public function createOrder(Cart $cart)
+    {
+    }
+}
