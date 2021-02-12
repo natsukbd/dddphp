1f43f6e82ddc2905f2452d66f1d9c8f3d35179f1
natsukbd <natsuki.pc@gmail.com>
2021-02-12 23:18:04 +0900

    ユーザ認証の責任を持つドメインサービス

---
+++ laravel/src/DDD/Services/Domain/Service/SignIn.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DDD\Services\Domain\Service;
+
+final class SignIn
+{
+    public function authenticate(string $username, string $password)
+    {
+
+    }
+}
